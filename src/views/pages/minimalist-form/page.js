c04ced09b7ccc8eba56d9e1f1f0780c6c9ad11b1
Caio Kinzel Filho <caiokf@gmail.com>
2017-11-10 13:08:26 +0400

    minimalist form credits

--- src/views/pages/minimalist-form/page.js
+++ src/views/pages/minimalist-form/page.js
@@ -24,7 +24,19 @@ export default class MinimalistFormPage extends Component {
     return (
       <Layout title="Minimalist Form Page" {...props}>
         <Container>
+          <Description>
+            A minimalist form interface that shows only one text input at a time,&nbsp;
+            transitioning to the next input with a subtle animation.&nbsp;
+            It has a progress bar and question counter as well.
+          </Description>
+
           <MinimalistForm questions={questions} />
+
+          <Description>
+            Inspired on this&nbsp;
+            <a href="https://github.com/codrops/MinimalForm" target="_blank">form</a>&nbsp;
+            and implemented in React & styled-components
+          </Description>
         </Container>
       </Layout>
     )
@@ -34,6 +46,10 @@ const Container = styled.div`
   background: ${colors.background};
   display: flex;
   flex: 1;
-  justify-content: center;
+  justify-content: space-evenly;
   flex-direction: column;
+  align-items: center;
+`
+const Description = styled.p`
+  max-width: 500px;
 `
